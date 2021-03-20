474a3380d19144556aabf4e80377ce7e43f8b265
Radhi Guennichi <guennichiradhi@gmail.com>
2021-03-20 22:06:32 +0100

    update copyrights

--- tests/GuennichiPerformistBundleTest.php
+++ tests/GuennichiPerformistBundleTest.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the PerformistBundle package.
  *
- * (c) Radhi GUENNICHI <radhi@guennichi.com> (https://www.guennichi.com)
+ * (c) Radhi GUENNICHI <hello@guennichi.com> (https://www.guennichi.com)
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
